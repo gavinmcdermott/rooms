d7ee66201ba50eea4f8e1f76c614f71b891550c2
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-11 00:43:22 -0700

    modify package to include meteor dependencies

--- package.js
+++ package.js
@@ -3,6 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function(api){
+  api.use('accounts-ui', ['client', 'server']);
+  api.use('accounts-password', ['client', 'server']);
+
   api.add_files('client.js', 'client');
   api.add_files('shared.js', ['client', 'server']);
 });
