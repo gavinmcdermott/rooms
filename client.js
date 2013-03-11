3bc8e5912b0dec3c2e0c1923a41f06e8bac96628
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-10 20:20:40 -0700

    wrap Room object in self invoking function

--- client.js
+++ client.js
@@ -1,51 +1,79 @@
-Room = {};
-Room.availableRooms = function() {
-  return Rooms.find({ current_count: { $gt: -1, $lt: ROOM_SIZE } }).fetch();
-};
-Room.availableRoomCount = function() {
-  return Rooms.find({ current_count: { $gt: -1, $lt: ROOM_SIZE } }).fetch().length;
-};
-Room.makeRoom = function(size, roomName, callback) {
-  var room = Rooms.insert({
-    'name': roomName || null,
-    'players': [],
-    'current_count': 0,
-    'size': size
-  }, function(error, result) {
-    callback(result);
-  });
-};
-Room.createSession = function(room) {
-  Session.set('currentRoom', room);
-};
-Room.addToRoom = function(room, user) {
-  Rooms.update( {_id: room},
-                {$inc: {current_count: 1} } );
-  Rooms.update( {_id: room},
-                {$push: {players: user} } );
-};
-Room.removeFromRoom = function(room, user) {
-  Players.update( {_id: user},
-                  { $set : { room_id: null } } );
-  Rooms.update( {_id: room},
-                {$inc: {current_count: -1} } );
+(function(){
 
-  var usersInRoom = Rooms.findOne({_id: room},
-                                  {players: 1, _id: 0} ).players;
-  var playerIdx = usersInRoom.indexOf(user);
-  playerIdx = 'players.'+playerIdx;
-  var playerLeaving = {};
-  playerLeaving[playerIdx] = 1;
-  Rooms.update( {_id: room},
-                {$unset: playerLeaving }, function(){
-                  Rooms.update( {_id: room},
-                  {$pull: {'players': null } }, function(){
-                    var r = Rooms.findOne({_id: room});
-                    if (!r.players.length) {
-                      Rooms.remove({_id: room});
-                    }
-                    Session.set('currentRoom', null);
-                    // console.log("the currentRoom is now: ", Session.get('currentRoom'));
+  Room = {};
+
+  var maxSize;
+
+  Room.setMax = function(size) {
+    maxSize = size;
+    return;
+  };
+
+  Room.availableRooms = function() {
+    return Rooms.find({ current_count: { $gt: -1, $lt: maxSize } }).fetch();
+  };
+
+  Room.availableRoomCount = function() {
+    return Rooms.find({ current_count: { $gt: -1, $lt: maxSize } }).fetch().length;
+  };
+
+  Room.makeRoom = function(callback, room_name) {
+
+    var roomSize;
+
+    if(maxSize) {
+      roomSize = maxSize;
+    } else {
+      roomSize = null;
+    }
+
+    Rooms.insert({
+      'current_count': 0,
+      'max_size': roomSize,
+      'name': room_name || null,
+      'users': []
+    }, function(error, roomId) {
+      callback(roomId);
+    });
+  };
+
+  Room.createSession = function(roomId) {
+    return Session.set('currentRoom', roomId);
+  };
+
+  Room.addToRoom = function(roomId, user) {
+    Rooms.update( {_id: roomId},
+                  {$inc: {current_count: 1} } );
+    Rooms.update( {_id: roomId},
+                  {$push: {users: user} } );
+  };
+
+  Room.removeFromRoom = function(roomId, user) {
+    Players.update( {_id: user},
+                    { $set : { room_id: null } } );
+    Rooms.update( {_id: roomId},
+                  {$inc: {current_count: -1} } );
+
+    var usersInRoom = Rooms.findOne({_id: roomId},
+                                    {users: 1, _id: 0} ).users;
+    var playerIdx = usersInRoom.indexOf(user);
+    playerIdx = 'users.'+playerIdx;
+    var playerLeaving = {};
+    playerLeaving[playerIdx] = 1;
+    Rooms.update( {_id: roomId},
+                  {$unset: playerLeaving }, function(){
+                    Rooms.update( {_id: roomId},
+                    {$pull: {'users': null } }, function(){
+                      var r = Rooms.findOne({_id: roomId});
+                      if (!r.users.length) {
+                        Rooms.remove({_id: roomId});
+                      }
+                      Session.set('currentRoom', null);
+                      // console.log("the currentRoom is now: ", Session.get('currentRoom'));
+                    } );
                   } );
-                } );
-};
+  };
+
+  return Room;
+
+}());
