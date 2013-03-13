0b7d35e0229323c0cf1747dc859fcf7910ca8c7a
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-12 21:48:51 -0700

    get things working after trying to split smart package

--- shared.js
+++ shared.js
@@ -3,7 +3,3 @@ var Rooms = new Meteor.Collection('rooms');
 var LoggedUsers = new Meteor.Collection('loggedUsers');
 
 var Messages = new Meteor.Collection('messages');
-
-//while client is open, send timestamp to server
-//update collection with timestamp
-//server function checking if timestamp has diff of over 10s
