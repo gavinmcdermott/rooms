020fbaed4a2ab408e88df6e5afb97c64221e3dd1
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-27 12:01:02 -0700

    change to room garbage collection

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ Meteor.setInterval(function(){
 
   var time = new Date().getTime() - 5000;
   var expiredUsers = LoggedUsers.find( {stamp: {$lt: time } } ).fetch();
-  // console.log('outer');
   for (var i = 0; i < expiredUsers.length; i++) {
-    // console.log('inner');
-    Rooms.update({}, {$pull: {'users': expiredUsers[i].id} }, function(e) {
-      console.log('success!!!');
-      console.log('we removed: ', expiredUsers[i].id);
+    Rooms.update({}, {$pull: {'users': expiredUsers[i].id} }, { multi: true }, function(e) {
+      // console.log('success!!!');
+      // console.log('we removed: ', expiredUsers[i].id);
     });
     LoggedUsers.remove({'id': expiredUsers[i].id});
   }
